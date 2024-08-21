f17f1f3649e8dd333f4b5fe67d8b2dd8d6ea030f
jberm0 <jmberman12@gmail.com>
2024-08-21 16:41:31 +0100

    format trusted files

---
+++ draft_run/trusted/league.py
@@ -0,0 +1,87 @@
+import polars as pl
+import sys
+
+sys.path.append("./././")
+
+from src.utils.input_output import read_parquet, write_parquet
+
+raw_path = "data/raw"
+trusted_path = "data/trusted"
+
+
+def get_standings(h2h_standings, entries):
+    standings = (
+        h2h_standings.join(
+            entries.select("id", "entry_name", "short_name"),
+            left_on="league_entry",
+            right_on="id",
+            how="inner",
+        )
+        .select(
+            "rank",
+            "last_rank",
+            pl.col("entry_name").alias("team_name"),
+            pl.col("short_name").alias("owner"),
+            "matches_won",
+            "matches_drawn",
+            "matches_lost",
+            "points_against",
+            "points_for",
+            pl.col("total").alias("total_points"),
+        )
+        .sort("rank", descending=False)
+    )
+
+    return standings
+
+
+def get_head_to_head(h2h_fixtures, entries):
+    head_to_head = (
+        h2h_fixtures.join(
+            entries.select(
+                "id",
+                pl.col("entry_name").alias("team_name_1"),
+                pl.col("short_name").alias("owner_1"),
+            ),
+            left_on="league_entry_1",
+            right_on="id",
+            how="inner",
+        )
+        .join(
+            entries.select(
+                "id",
+                pl.col("entry_name").alias("team_name_2"),
+                pl.col("short_name").alias("owner_2"),
+            ),
+            left_on="league_entry_2",
+            right_on="id",
+            how="inner",
+        )
+        .select(
+            "event",
+            "started",
+            "finished",
+            "league_entry_1",
+            "team_name_1",
+            "owner_1",
+            "league_entry_1_points",
+            "league_entry_2",
+            "team_name_2",
+            "owner_2",
+            "league_entry_2_points",
+        )
+    )
+
+    return head_to_head
+
+
+if __name__ == "__main__":
+    h2h_fixtures = read_parquet(f"{raw_path}/h2h_fixtures.parquet")
+    h2h_standings = read_parquet(f"{raw_path}/h2h_standings.parquet")
+    entries = read_parquet(f"{raw_path}/league_entries.parquet")
+
+    head_to_head = get_head_to_head(h2h_fixtures, entries)
+    standings = get_standings(h2h_standings, entries)
+
+    write_parquet(head_to_head, trusted_path + "/head_to_head.parquet")
+    write_parquet(standings, trusted_path + "/standings.parquet")
--- draft_run/trusted/scoring.py
+++ draft_run/trusted/scoring.py
@@ -9,28 +9,34 @@ from src.process.cleaning import rename_columns_by_regex
 raw_path = "data/raw"
 trusted_path = "data/trusted"
 
-scoring = read_json(f"{raw_path}/scoring.json")
 
-scoring_df = pl.DataFrame(scoring)
+def create_scoring(scoring):
+    scoring_df = pl.DataFrame(scoring)
 
-positions_list = ["GKP", "DEF", "MID", "FWD"]
+    positions_list = ["GKP", "DEF", "MID", "FWD"]
 
-standard_scoring = scoring_df.select(
-    pl.col("*").exclude(*[f"^*_{pos}*$" for pos in positions_list])
-)
-
-cols = ["goals_conceded", "goals_scored", "clean_sheets"]
-
-position_points_dfs = []
-for pos in positions_list:
-    df_r = (
-        scoring_df.select(pl.col(f"^*_{pos}*$"))
-        .pipe(rename_columns_by_regex, f"_{pos}", "")
-        .select(pl.lit(f"{pos}").alias("position"), *cols)
+    standard_scoring = scoring_df.select(
+        pl.col("*").exclude(*[f"^*_{pos}*$" for pos in positions_list])
     )
-    df_r = pl.concat([df_r, standard_scoring], how="horizontal")
-    position_points_dfs.append(df_r)
 
-position_scoring = pl.concat(position_points_dfs, how="vertical")
+    cols = ["goals_conceded", "goals_scored", "clean_sheets"]
 
-write_parquet(position_scoring, trusted_path + "/scoring.parquet")
+    position_points_dfs = []
+    for pos in positions_list:
+        df_r = (
+            scoring_df.select(pl.col(f"^*_{pos}*$"))
+            .pipe(rename_columns_by_regex, f"_{pos}", "")
+            .select(pl.lit(f"{pos}").alias("position"), *cols)
+        )
+        df_r = pl.concat([df_r, standard_scoring], how="horizontal")
+        position_points_dfs.append(df_r)
+
+    position_scoring = pl.concat(position_points_dfs, how="vertical")
+
+    return position_scoring
+
+
+if __name__ == "__main__":
+    scoring = read_json(f"{raw_path}/scoring.json")
+    position_scoring = create_scoring(scoring)
+    write_parquet(position_scoring, trusted_path + "/scoring.parquet")
