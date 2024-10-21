0d5404208091b411caeaa46795d830c4ba7367b0
jberm0 <jmberman12@gmail.com>
2024-10-21 23:47:20 +0100

    feat: clean up charts

--- pages/1_my_team.py
+++ pages/1_my_team.py
@@ -11,10 +11,19 @@ head_to_head = pl.read_parquet(f"{trusted_path}head_to_head.parquet")
 match_stats = pl.read_parquet(f"{trusted_path}match_stats.parquet")
 stats = pl.read_parquet(f"{trusted_path}stats.parquet")
 
+if "selected_options" not in st.session_state:
+    st.session_state["selected_options"] = []
 
-my_selections = selections.filter(pl.col("owner") == "JB").select(
+with st.sidebar:
+    owner_filter = st.selectbox(
+        label="Select Owner", options=["JB", "KO", "VA", "LJ", "GN", "SH", "MM", "FM"]
+    )
+    position_filter = st.selectbox("Select Position", ["GK", "DEF", "MID", "FWD"])
+
+selections = selections.filter(pl.col("owner") == owner_filter).select(
     "event", "team", "player"
 )
+
 
 player_id_position = players.select(
     "web_name",
@@ -24,13 +33,13 @@ player_id_position = players.select(
     .alias("position"),
 )
 
-st.write("# My Team")
+st.write("# Team Summary")
 
 st.markdown("## Selections")
 
 
-my_team = (
-    my_selections.join(
+team = (
+    selections.join(
         player_points,
         left_on=["event", "player", "team"],
         right_on=["event", "web_name", "short_name"],
@@ -62,23 +71,21 @@ my_team = (
     )
 )
 
-my_team = my_team.select(
-    my_team.select(pl.exclude("event", "player", "team", "position", "player_id"))
+team = team.select(
+    team.select(pl.exclude("event", "player", "team", "position", "player_id"))
     .sum_horizontal()
     .alias("total"),
-    *my_team.columns,
+    *team.columns,
 )
 
-by_position = my_team.group_by("event", "position").agg(pl.sum("total"))
+by_position = team.group_by("event", "position").agg(pl.sum("total"))
 st.line_chart(data=by_position, x="event", y="total", color="position")
 
 st.write("#### Drill down into position")
 
-position_filter = st.selectbox("Select Position", ["GK", "DEF", "MID", "FWD"])
-
-my_team = my_team.filter(pl.col("position") == position_filter)
+team = team.filter(pl.col("position") == position_filter)
 st.line_chart(
-    data=my_team.select("position", "event", "total", "player"),
+    data=team.select("position", "event", "total", "player"),
     x="event",
     y="total",
     color="player",
@@ -87,27 +94,27 @@ st.line_chart(
 
 st.write("## Record")
 
-my_points_diff = (
+points_diff = (
     head_to_head.filter(pl.col("finished"))
-    .filter((pl.col("owner_1") == "JB") | (pl.col("owner_2") == "JB"))
+    .filter((pl.col("owner_1") == owner_filter) | (pl.col("owner_2") == owner_filter))
     .select(
         "event",
-        pl.when(pl.col("owner_1") == "JB")
+        pl.when(pl.col("owner_1") == owner_filter)
         .then(pl.col("league_entry_1_points"))
         .otherwise(pl.col("league_entry_2_points"))
-        .alias("my_points"),
-        pl.when(pl.col("owner_1") == "JB")
+        .alias("points"),
+        pl.when(pl.col("owner_1") == owner_filter)
         .then(pl.col("league_entry_2_points"))
         .otherwise(pl.col("league_entry_1_points"))
         .alias("opp_points"),
     )
     .with_columns(
-        (pl.col("my_points") - pl.col("opp_points")).alias("diff"),
+        (pl.col("points") - pl.col("opp_points")).alias("diff"),
     )
     .with_columns((pl.when(pl.col("diff") >= 0).then(1).otherwise(0).alias("win")))
 )
 
-st.bar_chart(data=my_points_diff, x="event", y="diff", stack=True, color="win")
+st.bar_chart(data=points_diff, x="event", y="diff", stack=True, color="win")
 
 st.write("## Home and Away")
 
@@ -167,23 +174,90 @@ st.bar_chart(
 
 st.write("## Form Guide")
 
-st.write(stats)
+metrics = [
+    "minutes",
+    "goals_scored",
+    "assists",
+    "clean_sheets",
+    "goals_conceded",
+    "own_goals",
+    "penalties_saved",
+    "penalties_missed",
+    "yellow_cards",
+    "red_cards",
+    "saves",
+    "bonus",
+    "bps",
+    "influence",
+    "creativity",
+    "threat",
+    "ict_index",
+    "starts",
+    "expected_goals",
+    "expected_assists",
+    "expected_goal_involvements",
+    "expected_goals_conceded",
+    "total_points",
+]
 
-team_form = my_selections.join(
-    match_stats,
-    left_on=["event", "player", "team"],
-    right_on=["event", "name", "team_name"],
-    how="left",
-).join(
-    stats,
-    left_on=["event", "team", "element"],
-    right_on=["event", "web_name", "player_id"],
-    how="left",
+team_form = (
+    selections.join(
+        match_stats,
+        left_on=["event", "player", "team"],
+        right_on=["event", "name", "team_name"],
+        how="left",
+    )
+    .select(
+        "event",
+        "team",
+        "player",
+        "h_or_a",
+        "opp_name",
+        "team_scored",
+        "team_conceded",
+        "element",
+    )
+    .join(
+        stats,
+        left_on=["event", "team", "element"],
+        right_on=["event", "team", "player_id"],
+        how="left",
+    )
+    .join(
+        player_id_position.select("id", "position"),
+        left_on="element",
+        right_on="id",
+        how="inner",
+    )
+    .select(
+        "event",
+        "team",
+        "player",
+        "position",
+        "h_or_a",
+        "opp_name",
+        "team_scored",
+        "team_conceded",
+        "element",
+        *metrics,
+    )
 )
 
-st.write(team_form)
+metric_filter = st.multiselect(
+    label="Metric Filter", options=metrics, default="total_points"
+)
+metrics_cumulative = team_form.select(
+    "event",
+    "player",
+    *[
+        pl.col(metric).cum_sum().over("player").alias(f"cumulative_{metric}")
+        for metric in metric_filter
+    ],
+)
 
-
-# TODO:
-# form guide - who to get rid of
-# next fixtures difficulty
+st.line_chart(
+    data=metrics_cumulative,
+    x="event",
+    y=(f"cumulative_{metric}" for metric in metric_filter),
+    color="player",
+)
