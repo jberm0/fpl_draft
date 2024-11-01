30773dfa5fb766318de51f07ec92d69c0eaabb67
jberm0 <jmberman12@gmail.com>
2024-11-01 09:56:47 +0000

    feat: fixtures page

--- pages/2_fixtures.py
+++ pages/2_fixtures.py
@@ -12,6 +12,15 @@ head_to_head = pl.read_parquet(f"{trusted_path}head_to_head.parquet")
 match_stats = pl.read_parquet(f"{trusted_path}match_stats.parquet")
 upcoming_fixtures = pl.read_parquet(f"{trusted_path}upcoming_fixtures.parquet")
 
+st.write("# Fixture Difficulty")
+
+standings = pl.read_parquet(f"{trusted_path}standings.parquet").select(
+    (pl.col("matches_won") + pl.col("matches_drawn") + pl.col("matches_lost")).alias(
+        "P"
+    )
+)
+last_gw = pl.Series(standings.select(pl.first("P"))).to_list()[0]
+
 
 fixtures = fixtures.select(
     pl.col("event").cast(pl.Int32),
@@ -37,18 +46,12 @@ players_with_team = players.join(
     team_to_id, how="inner", left_on="team_id", right_on="team_id"
 )
 
-st.dataframe(players_with_team)
-
 selected_players = selections.join(
     players_with_team,
     how="inner",
     left_on=["team", "player"],
     right_on=["team", "web_name"],
 )
-
-st.write(selected_players)
-
-st.write(fixtures)
 
 # upcoming fixture rating - which team has the easiest/hardest next run of fixtures
 
@@ -64,13 +67,91 @@ a_fixtures = fixtures.select(
     pl.col("team_h").alias("opp"),
     pl.col("team_a_difficulty").alias("fdr"),
 )
-all_fixtures = (
-    pl.concat([h_fixtures, a_fixtures])
-    .join(team_to_id, how="inner", left_on="team_id", right_on="team_id")
-    .join(team_to_id, how="inner", left_on="opp", right_on="team_id")
-).select("event", "team", pl.col("team_right").alias("opp"), "fdr")
 
-st.write(all_fixtures)
+
+prem_teams, fpl_teams = st.tabs(["Prem Teams", "FPL Teams"])
+
+gw_filter = prem_teams.slider(
+    "Gameweek Filter",
+    min_value=1,
+    max_value=38,
+    value=(last_gw + 1, last_gw + 3),
+    step=1,
+)
+
+col1, col2 = prem_teams.columns(2)
+
+all_fixtures = (
+    (
+        pl.concat([h_fixtures, a_fixtures])
+        .join(team_to_id, how="inner", left_on="team_id", right_on="team_id")
+        .join(team_to_id, how="inner", left_on="opp", right_on="team_id")
+    )
+    .select("event", "team", pl.col("team_right").alias("opp"), "fdr")
+    .sort("event", "team")
+)
+
+all_fixtures_filtered = all_fixtures.filter(
+    pl.col("event").is_between(gw_filter[0], gw_filter[1])
+)
+
+
+easiest_fixtures = (
+    all_fixtures_filtered.group_by("team")
+    .agg(pl.sum("fdr").alias("total_fdr"), pl.col("opp").explode())
+    .sort(pl.col("total_fdr"), descending=False)
+    .limit(10)
+)
+col1.write("## Good runs")
+col1.write(easiest_fixtures)
+
+hardest_fixtures = (
+    all_fixtures_filtered.group_by("team")
+    .agg(pl.sum("fdr").alias("total_fdr"), pl.col("opp").explode())
+    .sort(pl.col("total_fdr"), descending=True)
+    .limit(10)
+)
+col2.write("## Tough runs")
+col2.write(hardest_fixtures)
 
 
 # who's teams have the easiest and hardest run of fixtures
+
+selections_difficulty = selected_players.select(
+    pl.col("event").cast(pl.Int32), "owner", "player", "team"
+).join(
+    all_fixtures.select(pl.col("event").cast(pl.Int32), "team", "opp", "fdr"),
+    how="left",
+    left_on=["event", "team"],
+    right_on=["event", "team"],
+)
+
+grouped_by_owner = selections_difficulty.group_by("event", "owner").agg(
+    pl.sum("fdr").alias("total_fdr")
+)
+fpl_teams.write("## Historic FDR by team")
+fpl_teams.line_chart(grouped_by_owner, x="event", y="total_fdr", color="owner")
+
+
+current_selections = selected_players.filter(pl.col("event") == last_gw)
+next_fixtures = all_fixtures.filter(pl.col("event") == last_gw + 1)
+
+selection_projection = current_selections.select("owner", "team", "player").join(
+    next_fixtures.select("team", "opp", "fdr"), how="inner", on="team"
+)
+
+fdr_projection = selection_projection.group_by("owner").agg(
+    pl.sum("fdr").alias("total_fdr")
+)
+fpl_teams.write(f"## FDR Projection for GW {last_gw+1} based on current squads")
+fpl_teams.bar_chart(
+    fdr_projection, x="owner", y="total_fdr", color="total_fdr", stack="center"
+)
+
+owner_filter = fpl_teams.selectbox(
+    label="Select Owner", options=["JB", "KO", "VA", "LJ", "GN", "SH", "MM", "FM"]
+)
+
+fpl_teams.write(f"## Squad Projection for GW {last_gw+1} based on current squads")
+opp_projection = selection_projection.filter(pl.col("owner") == owner_filter)
+fpl_teams.write(opp_projection)
